ebc675e1f4fdc9e50e924f9b8cfd51f82d1b927a
Wu Niutian <wuniutian@gmail.com>
2022-07-16 09:31:48 +0800

    Initial Commit

---
+++ Cats/CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsApp.swift
+//  Cats
+//
+//  Created by    wuniutian on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
